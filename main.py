86f972786c43c4fba4659373a5e3c827b92c3cc1
Oleg Zen <zenol@yandex.ru>
2023-01-22 15:48:53 +0300

    Refactoring docstring  with flake8

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from random import randint
+from graphics_arts.start_game_banner import run_screensaver
+
+
+from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Подсчет урона противнику."""
     if char_class == 'warrior':
         return ((f'{char_name} нанёс урон противнику равный'
                  f' {5 + randint(3, 5)}'))
@@ -15,6 +20,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Blocking enemy strikes."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +31,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Application of special skills."""
     if char_class == 'warrior':
         return ((f'{char_name} применил специальное умение «Выносливость'
                  f' {80 + 25}»'))
@@ -36,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Character descriptions."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +68,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choice character."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -81,7 +90,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -90,8 +100,4 @@ def main():
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-
-    print(start_training(char_name, char_class))
-
-
-main()
+    print(start_training(char_name, char_class)) 
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+import math
+
 def we_crash_all(name: str) -> str:    
     return 'Привет, ' + name + ', мы всё сломали!'
 
-print(we_crash_all(''))
+print(print.__doc__)
